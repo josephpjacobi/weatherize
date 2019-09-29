e8e5390b3ef0abbc5fe54aaca4be62e90dba3664
Joe Jacobi <joseph.p.jacobi@gmail.com>
2019-09-29 11:11:16 -0400

    Used static data from DarkSky API

---
+++ src/components/landing-page/landing-page.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+
+class LandingPage extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>Get the Weather Report For:</h1>
+        <form action="" method="get">???</form>
+        <button type='submit'>Get the Weather!</button>
+      </div>
+    )
+  }
+}
---
+++ src/components/weather/data.js
@@ -0,0 +1,60 @@
+export const weatherData = {
+  'New York':
+  {
+    'city': 'New York',
+    "latitude": 40.7381,
+    "longitude": -73.9712,
+    "timezone": "America/New_York",
+    "currently": {
+      "time": 1569514342,
+      "summary": "Partly Cloudy",
+      "icon": "partly-cloudy-day",
+      "nearestStormDistance": 13,
+      "nearestStormBearing": 179,
+      "precipIntensity": 0,
+      "precipProbability": 0,
+      "temperature": 78.78,
+      "apparentTemperature": 79.11,
+      "dewPoint": 63.07,
+      "humidity": 0.59,
+      "pressure": 1009.48,
+      "windSpeed": 8.19,
+      "windGust": 8.19,
+      "windBearing": 200,
+      "cloudCover": 0.14,
+      "uvIndex": 6,
+      "visibility": 10,
+      "ozone": 277.9
+    },
+    "offset": -4
+  },
+  'Boston':
+  {
+    'city': 'Boston',
+    "latitude": 42.3601,
+    "longitude": -71.0589,
+    "timezone": "America/New_York",
+    "currently": {
+        "time": 1569513871,
+        "summary": "Clear",
+        "icon": "clear-day",
+        "nearestStormDistance": 19,
+        "nearestStormBearing": 206,
+        "precipIntensity": 0,
+        "precipProbability": 0,
+        "temperature": 78.58,
+        "apparentTemperature": 78.58,
+        "dewPoint": 55.4,
+        "humidity": 0.45,
+        "pressure": 1008.8,
+        "windSpeed": 10.21,
+        "windGust": 10.21,
+        "windBearing": 206,
+        "cloudCover": 0.03,
+        "uvIndex": 6,
+        "visibility": 10,
+        "ozone": 286
+    },
+    "offset": -4
+  }
+}
