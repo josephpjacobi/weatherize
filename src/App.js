a4bf25fe5a5107afe7a3128def9cbe7414206be8
Joe Jacobi <joseph.p.jacobi@gmail.com>
2019-10-02 10:21:41 -0400

    added location data

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import './App.css';
 import { DisplayWeather } from './components/weather/display-weather';
+import { LandingPage } from './components/landing-page/landing-page';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
+      <div className="App"> 
+        <LandingPage />
         <DisplayWeather location='Boston'/>
       </div>
     );
